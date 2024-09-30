111a3112f4113aee27544911181b53ae60107756
lvxuan12 <lvxuan@big.ac.cn>
2024-09-30 10:27:30 +0800

    test

--- inst/shell/run_irfinder.sh
+++ inst/shell/run_irfinder.sh
@@ -5,12 +5,16 @@
 # $3:gtf file
 # $4:bam file directory
 # $5:core
+# $6:path to IRFinder
+# $7:path to samtools
 set -e
 workpath=$1
 ref=$2
 gtf=$3
 bampath=$4
 core=$5
+IRFinder_path=$6
+samtools_path=$7
 #export LD_LIBRARY_PATH="/disk/lvxuan/lib:$LD_LIBRARY_PATH"
 mkdir -p $workpath
 cd $workpath
@@ -23,7 +27,7 @@ fi
 mkdir $workpath/REF
 ln -s $ref $workpath/REF/genome.fa
 ln -s $gtf $workpath/REF/transcripts.gtf 
-IRFinder -m BuildRefProcess -r $workpath/REF
+${IRFinder_path} -m BuildRefProcess -r $workpath/REF
 for bam in `ls $bampath/*bam`
 do
     name=`echo $bam | awk -F'/' '{print $NF}' | sed 's/.bam//'`
@@ -33,8 +37,8 @@ do
         echo "$workpath/$name should not yet exist! Remove $workpath/REF"
     fi
     mkdir $workpath/$name
-    samtools sort -m 2G -n -@ $core -o $workpath/$name/${name}.bam $bam
-    IRFinder BAM -r $workpath/REF -d $workpath/$name $workpath/$name/${name}.bam && rm $workpath/$name/IRFinder-ChrCoverage.txt $workpath/$name/IRFinder-JuncCount.txt $workpath/$name/IRFinder-ROI.txt $workpath/$name/IRFinder-SpansPoint.txt $workpath/$name/${name}.bam
+    ${samtools_path} sort -m 2G -n -@ $core -o $workpath/$name/${name}.bam $bam
+    ${IRFinder_path} BAM -r $workpath/REF -d $workpath/$name $workpath/$name/${name}.bam && rm $workpath/$name/IRFinder-ChrCoverage.txt $workpath/$name/IRFinder-JuncCount.txt $workpath/$name/IRFinder-ROI.txt $workpath/$name/IRFinder-SpansPoint.txt $workpath/$name/${name}.bam
 done
 rm -r $workpath/REF
 
