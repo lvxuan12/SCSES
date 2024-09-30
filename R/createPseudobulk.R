a735cf481a454a9e2e4fdce5dd156a879342a819
lvxuan12 <lvxuan@big.ac.cn>
2024-09-30 15:50:25 +0800

    test

--- R/createPseudobulk.R
+++ R/createPseudobulk.R
@@ -9,28 +9,36 @@
 #' @export
 #'
 createPseudobulk <- function(
-    paras, bam_path = paras$Basic$bam_path, core = paras$Basic$core) {
-  #script
-  samtools_path = paras$Basic$samtools_path
-  # input
-  print(paste0("Input: ", bam_path))
-  # output
-  pseudobulk.path <- paste0(paras$Basic$work_path, "/data/")
-  print(paste0("Output: ", pseudobulk.path))
-  msg <- paste0("[", Sys.time(), "] ", "Creating Pseudobulk directory...")
-  print(msg)
-  dir.create(path = pseudobulk.path, recursive = T)
-  cmd <- paste(samtools_path, "merge", paste("-@", core), paste0(pseudobulk.path, "/all.bam"), paste0(bam_path, "/*.bam"), "--no-PG")
-  msg <- paste0("[", Sys.time(), "] ", "Merge Bam Files: ", cmd)
-  print(msg)
-  system(command = cmd, wait = T)
-  msg <- paste0("[", Sys.time(), "] ", "Merge Bam Files Finish.")
-  print(msg)
-  cmd <- paste(samtools_path, "index", paste0(pseudobulk.path, "/all.bam"))
-  msg <- paste0("[", Sys.time(), "] ", "Bam File Index: ", cmd)
-  print(msg)
-  system(command = cmd, wait = T)
-  msg <- paste0("[", Sys.time(), "] ", "Bam File Index Finish.")
-  print(msg)
-  return(pseudobulk.path)
+    paras, bam_path = paras$Basic$bam_path, core = paras$Basic$core,
+    overwrite = TRUE) {
+    #script
+    samtools_path = paras$Basic$samtools_path
+    # input
+    print(paste0("Input: ", bam_path))
+    # output
+    pseudobulk.path <- paste0(paras$Basic$work_path, "/data/")
+    print(paste0("Output: ", pseudobulk.path))
+    msg <- paste0("[", Sys.time(), "] ", "Creating Pseudobulk directory...")
+    print(msg)
+    dir.create(path = pseudobulk.path, recursive = T)
+    if(file.exists(paste0(pseudobulk.path, "/all.bam"))){
+        print("Pseudobulk bam file exists.")
+        if (overwrite) {
+            cmd <- paste(samtools_path, "merge", paste("-f -@", core), paste0(pseudobulk.path, "/all.bam"), paste0(bam_path, "/*.bam"), "--no-PG")
+        }
+    }else {
+        cmd <- paste(samtools_path, "merge", paste("-@", core), paste0(pseudobulk.path, "/all.bam"), paste0(bam_path, "/*.bam"), "--no-PG")
+    }
+    msg <- paste0("[", Sys.time(), "] ", "Merge Bam Files: ", cmd)
+    print(msg)
+    system(command = cmd, wait = T)
+    msg <- paste0("[", Sys.time(), "] ", "Merge Bam Files Finish.")
+    print(msg)
+    cmd <- paste(samtools_path, "index",paste("-@", core), paste0(pseudobulk.path, "/all.bam"))
+    msg <- paste0("[", Sys.time(), "] ", "Bam File Index: ", cmd)
+    print(msg)
+    system(command = cmd, wait = T)
+    msg <- paste0("[", Sys.time(), "] ", "Bam File Index Finish.")
+    print(msg)
+    return(pseudobulk.path)
 }
