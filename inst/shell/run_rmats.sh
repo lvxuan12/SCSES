ba3505b3bd84ff81da81e72c720b4bc4e849548a
lvxuan12 <lvxuan@big.ac.cn>
2024-12-05 15:06:00 +0800

    Update run_rmats.sh

--- inst/shell/run_rmats.sh
+++ inst/shell/run_rmats.sh
@@ -26,7 +26,7 @@ mkdir -p $workpath
 ls $bampath/*bam > $workpath/rMats.list
 sed -i ':t;N;s/\n/,/;b t' $workpath/rMats.list
 
-${rMATS_path} --b1 $workpath/rMats.list --gtf $gtf -t $pair --variable-read-length --readLength $readlength --nthread $core --od $workpath/output --tmp $workpath/tmp
+bash ${rMATS_path} --b1 $workpath/rMats.list --gtf $gtf -t $pair --variable-read-length --readLength $readlength --nthread $core --od $workpath/output --tmp $workpath/tmp
 rm -r $workpath/tmp
 rm -r $workpath/output/tmp
 rm $workpath/output/fromGTF* $workpath/output/JC*
