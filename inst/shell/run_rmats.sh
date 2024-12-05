91b8fe8ebcececf2d4d54e711f4f5445f2fe8790
lvxuan12 <lvxuan@big.ac.cn>
2024-12-05 14:55:36 +0800

    Update run_rmats.sh

--- inst/shell/run_rmats.sh
+++ inst/shell/run_rmats.sh
@@ -16,14 +16,14 @@ pair=$4
 readlength=$5
 core=$6
 rMATS_path=$7
-python_path=$8
+rMATS_env=$8
+conda_binpath=$9
 mkdir -p $workpath
 
 ls $bampath/*bam > $workpath/rMats.list
 sed -i ':t;N;s/\n/,/;b t' $workpath/rMats.list
 
-${python_path} ${rMATS_path} --b1 $workpath/rMats.list --gtf $gtf -t $pair --variable-read-length \
-        --readLength $readlength --nthread $core --od $workpath/output --tmp $workpath/tmp
+${rMATS_path} --b1 $workpath/rMats.list --gtf $gtf -t $pair --variable-read-length --readLength $readlength --nthread $core --od $workpath/output --tmp $workpath/tmp
 rm -r $workpath/tmp
 rm -r $workpath/output/tmp
 rm $workpath/output/fromGTF* $workpath/output/JC*
